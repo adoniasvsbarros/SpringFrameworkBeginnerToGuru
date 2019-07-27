c73f76fe48bd0dc5bad9f3b9d4da5bf50e12a55c
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-07-26 21:52:09 -0300

    Refactoring image service, controller, and tests with more reactive

--- section21/spring5-reactive-mongo-recipe-app/src/test/java/guru/springframework/services/ImageServiceImplTest.java
+++ section21/spring5-reactive-mongo-recipe-app/src/test/java/guru/springframework/services/ImageServiceImplTest.java
@@ -1,7 +1,9 @@
 package guru.springframework.services;
 
 import guru.springframework.domain.Recipe;
-import guru.springframework.repositories.RecipeRepository;
+import guru.springframework.repositories.reactive.RecipeReactiveRepository;
+import reactor.core.publisher.Mono;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -9,49 +11,45 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.Optional;
-
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
-
 
 public class ImageServiceImplTest {
 
-    @Mock
-    RecipeRepository recipeRepository;
+	@Mock
+	RecipeReactiveRepository recipeReactiveRepository;
 
-    ImageService imageService;
+	ImageService imageService;
 
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		imageService = new ImageServiceImpl(recipeReactiveRepository);
+	}
 
-        imageService = new ImageServiceImpl(recipeRepository);
-    }
+	@Test
+	public void saveImageFile() throws Exception {
+		// given
+		String id = "1";
+		MultipartFile multipartFile = new MockMultipartFile("imagefile", "testing.txt", "text/plain",
+				"Spring Framework Guru".getBytes());
 
-    @Test
-    public void saveImageFile() throws Exception {
-        //given
-        String id = "1";
-        MultipartFile multipartFile = new MockMultipartFile("imagefile", "testing.txt", "text/plain",
-                "Spring Framework Guru".getBytes());
+		Recipe recipe = new Recipe();
+		recipe.setId(id);
 
-        Recipe recipe = new Recipe();
-        recipe.setId(id);
-        Optional<Recipe> recipeOptional = Optional.of(recipe);
+		when(recipeReactiveRepository.findById(anyString())).thenReturn(Mono.just(recipe));
+		when(recipeReactiveRepository.save(any(Recipe.class))).thenReturn(Mono.just(recipe));
 
-        when(recipeRepository.findById(anyString())).thenReturn(recipeOptional);
+		ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
 
-        ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+		// when
+		imageService.saveImageFile(id, multipartFile).block();
 
-        //when
-        imageService.saveImageFile(id, multipartFile);
-
-        //then
-        verify(recipeRepository, times(1)).save(argumentCaptor.capture());
-        Recipe savedRecipe = argumentCaptor.getValue();
-        assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
-    }
+		// then
+		verify(recipeReactiveRepository, times(1)).save(argumentCaptor.capture());
+		Recipe savedRecipe = argumentCaptor.getValue();
+		assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
+	}
 
 }
